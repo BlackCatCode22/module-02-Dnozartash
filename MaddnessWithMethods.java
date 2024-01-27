dc1b358ec513e130ba17757322b2b1d1c32a7fe9
Dnozartash <155946733+Dnozartash@users.noreply.github.com>
2024-01-27 00:09:44 -0800

    Add files via upload

---
+++ MaddnessWithMethods.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class MaddnessWithMethods {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var num1 = getAnIntFromTheUser(scanner);
+        int num2 = getAnIntFromTheUser(scanner);
+
+        int sum = sumTwoInts(num1, num2);
+        int comparison = Integer.parseInt(compareTwoInts(num1, num2));
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        System.out.println(num1 + " is " + comparison + " than " + num2);
+    }
+
+    // Method to get an integer from the user
+    public static int getAnIntFromTheUser(Scanner scanner) {
+        System.out.print("Please enter an integer: ");
+        return scanner.nextInt();
+    }
+
+    // Method to compare two integers
+    public static String compareTwoInts(int num1, int num2) {
+        if (num1 > num2) return "greater than";
+        else if (num1 < num2) {
+            return "less than";
+        } else {
+            return "equal to";
+        }
+    }
+
+    // Method to sum two integers
+    public static int sumTwoInts(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
--- Main.java
+++ Main.java
@@ -1,15 +1,37 @@
-            public class Main {
-            public static void main(String[] args) {
+import java.util.Scanner;
 
-                String str = "Hello World";
-                String result = "";
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = getAnIntFromTheUser(scanner);
+        int num2 = getAnIntFromTheUser(scanner);
 
-                for (int i = str.length() - 1; i > 0; i--)
-                    result += str.charAt(i);
+        int sum = sumTwoInts(num1, num2);
+        String comparison = compareTwoInts(num1, num2);
 
-                System.out.println("\n" + "Golden Reverse: " + result);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        System.out.println(num1 + " is " + comparison + " than " + num2);
+    }
 
+    // Method to get an integer from the user
+    public static int getAnIntFromTheUser(Scanner scanner) {
+        System.out.print("Please enter an integer: ");
+        return scanner.nextInt();
+    }
 
-
+    // Method to compare two integers
+    public static String compareTwoInts(int num1, int num2) {
+        if (num1 > num2) {
+            return "greater than";
+        } else if (num1 < num2) {
+            return "less than";
+        } else {
+            return "equal to";
         }
     }
+
+    // Method to sum two integers
+    public static int sumTwoInts(int num1, int num2) {
+        return num1 + num2;
+    }
+}
